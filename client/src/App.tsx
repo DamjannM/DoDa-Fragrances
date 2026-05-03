6f3f41bf55eb24af5f60ca6434edb7de7eab818d
DamjannM <damjanmilosavljevic@gmail.com>
2026-05-03 11:38:17 +0200

    bugfix

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import Register from "./components/Register";
 import Login from "./components/Login";
 import Home from "./components/Home";
