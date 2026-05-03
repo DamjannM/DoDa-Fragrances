90f74ffa954eefc05291cd5221bc9dc3498081e2
DamjannM <damjanmilosavljevic@gmail.com>
2026-05-03 11:22:17 +0200

    bugfix

--- server/src/routes/authRoutes.ts
+++ server/src/routes/authRoutes.ts
@@ -86,7 +86,7 @@ router.post("/login", async (req, res) => {
     res.cookie("token", token, {
       httpOnly: true,
       secure: process.env.NODE_ENV === "production",
-      sameSite: "lax",
+      sameSite: "none",
       maxAge: rememberMe ? 30 * 24 * 60 * 60 * 1000 : 2 * 60 * 60 * 1000,
     });
 
